54ac0d443d31e615cb62037ff9ddcb91abfb507f
Matéo LEROY <mateoleroy084@gmail.com>
2026-05-28 16:38:29 +0200

    trampoline & pause menu

--- Assets/_Content/Script/MenuManager.cs
+++ Assets/_Content/Script/MenuManager.cs
@@ -5,7 +5,7 @@ public class MenuManager : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene("Player");
+        SceneManager.LoadScene("FinishLineScene");
     }
 
     public void OpenSettings()
