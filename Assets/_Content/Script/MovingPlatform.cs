0915257be97a8c8fb7eb23e71c97add0d7d44993
cell-_-gaming <elowanpaino@gmail.com>
2026-05-28 15:32:04 +0200

    stage 2

---
+++ Assets/_Content/Script/MovingPlatform.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speed = 3f;
+    
+    private int currentWaypointIndex = 0;
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < 0.1f)
+        {
+            currentWaypointIndex++;
+            
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(
+            transform.position, 
+            waypoints[currentWaypointIndex].position, 
+            speed * Time.deltaTime
+        );
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
