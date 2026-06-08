8094d7dd33d28aafdeda41af25bccccd88142d78
Matéo LEROY <mateoleroy084@gmail.com>
2026-06-08 15:35:38 +0200

    niveau2 fini

--- Assets/_Content/Script/FallingPlatform.cs
+++ Assets/_Content/Script/FallingPlatform.cs
@@ -5,9 +5,18 @@ public class FallingPlatform : MonoBehaviour
     [Range(0f, 5f)]
     public float fallDelay = 2f;
     public float fallSpeed = 10f;
+    public float riseSpeed = 25f;
+    public float resetDelay = 7f;
 
     private bool isFalling = false;
+    private bool isRising = false;
     private bool hasBeenTriggered = false;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
 
     void OnTriggerEnter(Collider collision)
     {
@@ -21,6 +30,13 @@ public class FallingPlatform : MonoBehaviour
     void StartFalling()
     {
         isFalling = true;
+        Invoke(nameof(StartRising), resetDelay);
+    }
+
+    void StartRising()
+    {
+        isFalling = false;
+        isRising = true;
     }
 
     void Update()
@@ -28,6 +44,15 @@ public class FallingPlatform : MonoBehaviour
         if (isFalling)
         {
             transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+        }
+        else if (isRising)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, initialPosition, riseSpeed * Time.deltaTime);
+            if (transform.position == initialPosition)
+            {
+                isRising = false;
+                hasBeenTriggered = false;
+            }
         }
     }
 }
